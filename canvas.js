84868ff5ab3309f2bf0d73d6d901ec92fba3bbe2
August Toman-Yih <atomanyih@pivotal.io>
2015-08-11 10:47:03 -0700

    refactorbation

--- canvas.js
+++ canvas.js
@@ -2,30 +2,42 @@ var context = document.getElementById('canvas').getContext('2d');
 var slider = document.getElementById('slider');
 var width = 256;
 var height = 256;
-var imageData = context.getImageData(0,0, width, height);
+var image = new PixelCanvas(context);
+
+function PixelCanvas(context) {
+  var imageData = context.getImageData(0, 0, width, height);
+
+  return {
+    fillPixel: function (x, y, color) {
+      var index = (y * width + x) * 4;
+      imageData.data[index] = color.red;
+      imageData.data[index + 1] = color.green;
+      imageData.data[index + 2] = color.blue;
+      imageData.data[index + 3] = color.alpha * 255;
+    },
+    render: function () {
+      context.putImageData(imageData, 0, 0)
+
+    }
+  }
+}
 
 function drawCanvas() {
   var z = slider.value;
 
-  //context.clearRect(0, 0, width, height);
-
   for (var x = 0; x < width; x++) {
     for (var y = 0; y < height; y++) {
 
-      var index = (y * width + x) * 4;
 
       var otherColorFunction = xyzToRGBA(z);
       var color = otherColorFunction(x / width, y / height);
 
 
-      imageData.data[index] = color.red;
-      imageData.data[index + 1] = color.green;
-      imageData.data[index + 2] = color.blue;
-      imageData.data[index + 3] = color.alpha * 255;
+      image.fillPixel(x, y, color);
     }
   }
 
-  context.putImageData(imageData, 0, 0)
+  image.render();
 }
 
 drawCanvas();
