28ca391582f5052ba0894c1169b1366e23aac6af
August Toman-Yih <atomanyih@pivotal.io>
2015-08-11 12:00:41 -0700

    add hsv to rgba

--- color.js
+++ color.js
@@ -116,6 +116,41 @@ function Lab(l, a, b) {
   }
 }
 
+function HSV(H,S,V) {
+  return {
+    toRGB: function() {
+      if ( S == 0 )                       //HSV from 0 to 1
+      {
+        R = V * 255
+        G = V * 255
+        B = V * 255
+      }
+      else
+      {
+        var_h = H * 6
+        if ( var_h == 6 ) var_h = 0      //H must be < 1
+        var_i = Math.floor( var_h )             //Or ... var_i = floor( var_h )
+        var_1 = V * ( 1 - S )
+        var_2 = V * ( 1 - S * ( var_h - var_i ) )
+        var_3 = V * ( 1 - S * ( 1 - ( var_h - var_i ) ) )
+
+        if      ( var_i == 0 ) { var_r = V     ; var_g = var_3 ; var_b = var_1 }
+        else if ( var_i == 1 ) { var_r = var_2 ; var_g = V     ; var_b = var_1 }
+        else if ( var_i == 2 ) { var_r = var_1 ; var_g = V     ; var_b = var_3 }
+        else if ( var_i == 3 ) { var_r = var_1 ; var_g = var_2 ; var_b = V     }
+        else if ( var_i == 4 ) { var_r = var_3 ; var_g = var_1 ; var_b = V     }
+        else                   { var_r = V     ; var_g = var_1 ; var_b = var_2 }
+
+        R = var_r * 255                  //RGB results from 0 to 255
+        G = var_g * 255
+        B = var_b * 255
+      }
+
+      return new Color(R, G, B, 1);
+    }
+  }
+}
+
 var xyzToRGBA = function(y) {
   return function(x, z) {
     return new XYZ(x, y, z).toRGB();
@@ -125,5 +160,11 @@ var xyzToRGBA = function(y) {
 var labToRGBA = function(L) {
   return function(a, b) {
     return new Lab(L, a, b).toRGB();
+  }
+};
+
+var hsvtoRGBA = function(v) {
+  return function(h, s) {
+    return new HSV(h, s, v).toRGB();
   }
 };
